b4bb67997ab6edb7ebfaf469928ac5033c7acb05
Felipe Resende <feliperesendehq@outlook.com>
2017-04-22 15:57:33 -0300

    Configurando arquivos estáticos

--- qomic_plataform/urls.py
+++ qomic_plataform/urls.py
@@ -19,3 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
