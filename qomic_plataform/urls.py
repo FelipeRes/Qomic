1d629bdff0c87dfe311e2accb7a582bc45be5a6d
Felipe Resende <feliperesendehq@outlook.com>
2017-04-22 16:37:00 -0300

    Finalizando configuracao de urls

--- qomic_plataform/urls.py
+++ qomic_plataform/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from core import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', views.index),
+    url(r'^$', include('core.urls')),
 ]
