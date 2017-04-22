2719c207f627a4da57ec36b3250f6a3900f5a2c5
Felipe Resende <feliperesendehq@outlook.com>
2017-04-22 00:42:40 -0300

    Primeiro commit. Submentendo base da plataforma para o heroku

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\felipe~1\desktop\trabal~2\qomic\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ qomic_plataform/qomic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QomicConfig(AppConfig):
+    name = 'qomic'
