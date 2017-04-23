cd55cd812d8cd90271719e721a5d070645b05f96
Felipe Resende <feliperesendehq@outlook.com>
2017-04-23 03:50:17 -0300

    Criada classe obra

--- core/models.py
+++ core/models.py
@@ -1,13 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-'''
-class Usuario(models.Model):
-	nomeUsuario = models.CharField(max_length=50)
 
 class Obra(models.model):
+	usuario = models.ForeignKey(User)
 	nomeObra = models.CharField(max_length=50)
-
+	sinopse = models.CharField(max_length=500)
+	ativada = models.BooleanField(initial=True)
 	def __str__(self):
-		return self.status
-'''
+		return self.nomeObra
