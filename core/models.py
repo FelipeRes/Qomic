04e11a885bbf4a691535d46edab44405417aa888
Felipe Resende <feliperesendehq@outlook.com>
2017-04-23 03:52:07 -0300

    Corrigindo classe obra

--- core/models.py
+++ core/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Obra(models.model):
+class Obra(models.Model):
 	usuario = models.ForeignKey(User)
 	nomeObra = models.CharField(max_length=50)
 	sinopse = models.CharField(max_length=500)
-	ativada = models.BooleanField(initial=True)
+	ativada = models.BooleanField()
 	def __str__(self):
 		return self.nomeObra
